383100d8563df0e1525704936dc5959fb1d41410
Damian Ubowski <damian.ubowski.0@gmail.com>
2021-11-28 20:49:41 +0100

    Send broadcast intent from notification

--- app/src/main/java/com/s24083/shoppinglistnotificator/receivers/AddItemBroadcastReceiver.kt
+++ app/src/main/java/com/s24083/shoppinglistnotificator/receivers/AddItemBroadcastReceiver.kt
@@ -1,5 +1,6 @@
 package com.s24083.shoppinglistnotificator.receivers
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -11,14 +12,18 @@ import com.s24083.shoppinglistnotificator.R
 
 class AddItemBroadcastReceiver : BroadcastReceiver() {
 
-    var id = 0
+    private var id = 0
+    private val requestCode = 1
 
     override fun onReceive(context: Context, intent: Intent) {
+        val editItemIntent = Intent("com.s24083.shoppinglist.ITEM_ADDED_RESPONSE")
+        editItemIntent.putExtra("id", intent.getIntExtra("id", 0))
+        val pendingIntent = PendingIntent.getBroadcast(context, requestCode, editItemIntent, PendingIntent.FLAG_UPDATE_CURRENT)
         var builder = NotificationCompat.Builder(context, context.getString(R.string.channelID))
             .setSmallIcon(R.drawable.notification_icon)
-            .setContentTitle("New shopping item")
-            .setContentText("New shopping item has been created")
+            .setContentTitle("New shopping item ${intent.getStringExtra("name")}")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .addAction(R.mipmap.ic_launcher, "Edit", pendingIntent)
         with(NotificationManagerCompat.from(context)) {
             // notificationId is a unique int for each notification that you must define
             notify(id++, builder.build())
